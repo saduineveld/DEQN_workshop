b5f4ea314dedec80bc7294333b2b6307f0cacc4f
Sijmen Duineveld <s.a.duineveld@outlook.com>
2024-11-08 09:40:48 +0100

    RBC Equations & Definitions updated

--- Day_2/DEQN_library/RBC/Definitions.py
+++ Day_2/DEQN_library/RBC/Definitions.py
@@ -1,6 +1,8 @@
+import tensorflow as tf
+import numpy as np
 import Parameters
-import PolicyState
 import State
+import PolicyState
 
 alpha, beta, chi, delta, eta, nu  = Parameters.alpha, Parameters.beta, Parameters.chi, Parameters.delta,Parameters.eta, Parameters.nu
 
@@ -16,17 +18,48 @@ def get_ss():
 # Next periods capital
 def get_Kn(state, policy_state):
     """ Capital in next period, given state & policy """
-    _Kt = State.K_t(state)
+    _Kt = get_Kt(state)
     _Zt = get_Zt(state)
-    _Ct = PolicyState.C_t(policy_state)
+    _Ct = get_Ct(policy_state)
+
     _Ht = get_Ht(_Zt,_Kt,_Ct)
-    _Kn = prod(_Zt,_Kt,_Ht) + (1-delta)*_Kt - _Ct
+    _Yt = get_Yt(_Zt,_Kt,_Ht)
+
+    _Kn = _Yt + (1-delta)*_Kt - _Ct
     return _Kn
+
+# RHS of Euler in period t(!!)
+def get_RHSt(state, policy_state):
+    """ RHS in period t!! (given state & policy) """
+    _Kt = get_Kt(state)
+    _Zt = get_Zt(state)
+    _Ct = get_Ct(policy_state)
+
+    _Ht = get_Ht(_Zt,_Kt,_Ct)
+    _Yt = get_Yt(_Zt,_Kt,_Ht)
+
+    _Rt = get_Rt(_Yt,_Kt)
+    _RHSt = beta*marg_ut(_Ct)*(_Rt + 1 - delta)
+    return _RHSt
+
+
+def get_Kt(state):
+    """Take exponent of log(Kt)"""
+    _Kt = tf.math.exp(State.lKt(state))
+    return _Kt
 
 def get_Zt(state):
     """Take exponent of log(Zt)"""
-    _Zt = tf.math.exp(lZt(state))
+    _Zt = tf.math.exp(State.lZt(state))
     return _Zt
+
+def get_Ct(policy_state):
+    """Take exponent of log(Ct)"""
+    _Ct = tf.math.exp(PolicyState.lCt(policy_state))
+    return _Ct
+
+
+
 
 # Model subfunctions (no state or policy)
 def get_Ht(Zt,Kt,Ct):
@@ -38,12 +71,16 @@ def marg_ut(Ct):
     dUdC = Ct**-nu
     return dUdC
 
-def get_Ct(Zt,Kt,Ht,Kn):
-    Ct = prod(Zt,Kt,Ht) + (1-delta)*Kt - Kn
-    return Ct
-
-def prod(Zt,Kt,Ht): 
+def get_Yt(Zt,Kt,Ht): 
     Yt = Zt*Kt**alpha*Ht**(1-alpha)
-    #Rt = alpha*Yt/Kt
+    #
     #Wt = (1-alpha)*Yt/Ht
     return Yt#,Rt,Wt
+
+def get_Rt(Yt,Kt):
+    _Rt = alpha*Yt/Kt
+    return _Rt
+
+#def get_Ct(Zt,Kt,Ht,Kn):
+#    Ct = prod(Zt,Kt,Ht) + (1-delta)*Kt - Kn
+#    return Ct
--- Day_2/DEQN_library/RBC/Equations.py
+++ Day_2/DEQN_library/RBC/Equations.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-import Definitions as Df
+import Definitions
 import State
-import PolicyState as PS
+import PolicyState
 import Parameters
 
 alpha, beta, chi, delta, eta, nu  = Parameters.alpha, Parameters.beta, Parameters.chi, Parameters.delta,Parameters.eta, Parameters.nu
@@ -9,15 +9,23 @@ alpha, beta, chi, delta, eta, nu  = Parameters.alpha, Parameters.beta, Parameter
 def equations(state, policy_state):
     """Define the dictionary of loss functions."""
 
-    E_t = State.E_t_gen(state, policy_state)
-
-    Zt = Df.get_Zt(state)
-
-    Kt = State.Kt(state)
-
-    Ct = PS.Ct(policy_state)
+    # State variables (t)
+    #Zt = Definitions.get_Zt(state)
+    #Kt = Definitions.get_Kt(state)
+    # Policy (t)
+    Ct = Definitions.get_Ct(PolicyState)
 
     loss_dict = {}
+
+    E_t = State.E_t_gen(state, policy_state)
+    # Compute expecation of RHS of Euler equation
+    RHS_Eul = E_t(lambda s, ps: Definitions.get_RHSt(s,ps))
+
+    loss_dict['REE'] = RHS_Eul/Definitions.marg_ut(Ct) - 1
+
+    return loss_dict
+
+    
 
 
 
