e692ff6266749eebfc8cbeb94e0ef5632241ed62
Sijmen Duineveld <s.a.duineveld@outlook.com>
2024-11-19 18:00:35 +0100

    post process bm1972-adj updated

--- Day_2/DEQN_library/post_process_bm1972_adj.py
+++ Day_2/DEQN_library/post_process_bm1972_adj.py
@@ -34,13 +34,12 @@ nodes = 15
 K_bnds = tf.math.exp([tf.math.log(Kss)-lK_dev,tf.math.log(Kss)+lK_dev])
 K_plot = tf.linspace(K_bnds[0],K_bnds[1], nodes)
 K_plot = tf.reshape(K_plot,[nodes,1])
-K_plot = tf.Variable(K_plot)
-tf.print("K_plot",K_plot)
+print("K_plot",K_plot)
 tf.print("K_plot",K_plot.shape)
 
 # Consumption:
 s_plot = Parameters.policy(K_plot)
-tf.print("s_plot",s_plot)
+print("s_plot",s_plot)
 Y_plot = K_plot ** alpha
 C_plot = (1-s_plot)*Y_plot
 #plt.plot(K_plot,C_plot)
@@ -48,73 +47,26 @@ C_plot = (1-s_plot)*Y_plot
 tf.print("C_plot",C_plot)
 #os.system("pause")
 
-# Alternative computation
+### Sun simulation from different starting states ###
+lng = 10#lenght of simulation
+batches = 5#number of paths
 
+# Set state-array (pre-allocate lng x batches x state variables) + set initial values of first column vector (note row indices are second dimension, hence inidices [[0]])
+sim_state = tf.zeros([lng,batches,1])
+ini_state = tf.linspace(K_bnds[0],K_bnds[1],batches)#1 dim vector
+ini_state = tf.cast(ini_state,tf.float32)#Change type from float64 to float32
+ini_state = tf.reshape(ini_state,[1,batches,1])
+indices_split = tf.constant([[0]])
+sim_state = tf.tensor_scatter_nd_update(sim_state,indices_split,ini_state)
+#print("Input of simulation:",sim_state)
 
+# Get simulation of state variables
+sim_state = run_episode(sim_state)
+print("Output of simulation:",sim_state)
 
-tf.get_logger().setLevel('CRITICAL')
+# Reshape in 2D matrix (for each state variable, but there's oly one here):
+sim_k = tf.reshape(sim_state,[-1,batches])
+print("State in 2d matrix",sim_k)
+sim_k_df = pd.DataFrame(sim_k)
+sim_k_df.to_csv(Parameters.LOG_DIR + "/sim_k.csv", index=False)
 
-pd.set_option('display.max_columns', None)
-starting_policy = Parameters.policy(Parameters.starting_state)
-
-Equations = importlib.import_module(Parameters.MODEL_NAME + ".Equations")
-
-
-# Simulate single path        
-Parameters.initialize_each_episode = False        
-if not Parameters.initialize_each_episode:
-    ## simulate from a single starting state which is the mean across the N_sim_batch individual trajectory starting states
-    simulation_starting_state = tf.math.reduce_mean(Parameters.starting_state, axis = 0, keepdims=True)
-    ## simulate a long range and calculate variable bounds + means from it for plotting
-    print("Running a long simulation path")
-    N_simulated_episode_length = 100 #Parameters.N_episode_length or 10000
-    N_simulated_batch_size = 1 # Parameters.N_batch_size or 1
-        
-state_episode = tf.tile(tf.expand_dims(simulation_starting_state, axis = 0), [N_simulated_episode_length, 1, 1])
-
-print(state_episode.shape)
-
-
-print("Running episode to get range of variables...")
-# we are not going to re-run this graph, so let's not trace it
-tf.config.experimental_run_functions_eagerly(True)
-
-state_episode = tf.reshape(run_episode(state_episode), [N_simulated_episode_length * N_simulated_batch_size,len(Parameters.states)])
-
-print(state_episode.shape)
-
-mean_states = tf.math.reduce_mean(state_episode, axis = 0, keepdims=True)
-min_states = tf.math.reduce_min(state_episode, axis = 0)
-max_states = tf.math.reduce_max(state_episode, axis = 0)
-
-        
-print("Finished run. Calculating Euler discrepancies...")
-
-## calculate euler deviations
-policy_episode = Parameters.policy(state_episode)
-euler_discrepancies = pd.DataFrame(Equations.equations(state_episode, policy_episode))
-print("Euler discrepancy (absolute value) metrics")
-print(euler_discrepancies.abs().describe(include='all'))
-
-# save all relevant quantities along the trajectory 
-euler_discrepancies.to_csv(Parameters.LOG_DIR + "/simulated_euler_discrepancies.csv", index=False)
-
-state_episode_df = pd.DataFrame({s:getattr(State,s)(state_episode) for s in Parameters.states})
-state_episode_df.to_csv(Parameters.LOG_DIR + "/simulated_states.csv", index=False)
-
-policy_episode_df = pd.DataFrame({ps:getattr(PolicyState,ps)(policy_episode) for ps in Parameters.policy_states})
-policy_episode_df.to_csv(Parameters.LOG_DIR + "/simulated_policies.csv", index=False)
-
-definition_episode_df = pd.DataFrame({d:getattr(Definitions,d)(state_episode, policy_episode) for d in Parameters.definitions})
-definition_episode_df.to_csv(Parameters.LOG_DIR + "/simulated_definitions.csv", index=False)
-
-print("State metrics")
-print(state_episode_df.describe(include='all'))
-
-print("Policy metrics")
-print(policy_episode_df.describe(include='all'))
-
-print("Definition metrics")
-print(definition_episode_df.describe(include='all'))
-
-del sys.modules['Parameters']
---
+++ ToDo.md
@@ -0,0 +1,18 @@
+# To Do List
+
+## Test evaluation of policy function
+- BM: multiple simulations: save one file per variable (each path is a column or row)
+- RBC_noquad: save one file per variable (each path is a colum or row)
+
+## Solve RBC model with DEQN (no quadrature)
+- Make new RBC files for to DEQN format
+
+## Add model description of bm1972
+
+##  Write Manual for the DEQN library
+
+## Make new repo: DEQN-SD
+- add references/citations (and include papers)
+
+
+## Add stochasticity in Projeciton + DEQN
